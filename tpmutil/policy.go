2ef060bbf3c5115ac4dfd8e0fce9b983b03bc216
loicsikidi <loic.sikidi@gmail.com>
2026-04-04 22:59:26 +0200

    feat(tpmutil): add EkPolicyBViaACallback func
    Note: this commit also renamed EkPolicyCallback into EkPolicyACallback

--- tpmutil/policy.go
+++ tpmutil/policy.go
@@ -10,21 +10,54 @@ import (
 	"github.com/google/go-tpm/tpm2/transport"
 )
 
-// EkPolicyCallback is a callback used to satisfy EK's authPolicy.
+// EkPolicyACallback is a callback used to satisfy EK's PolicyA.
 // It is used to create a policy session for the endorsement hierarchy.
 //
 // Example:
 //
 //	activateRsp, err := tpm2.ActivateCredential{
-//		KeyHandle: tpmutil.ToAuthHandle(ekHandle, tpm2.Policy(tpm2.TPMAlgSHA256, 16, tpmutil.EkPolicyCallback)),
+//		KeyHandle: tpmutil.ToAuthHandle(ekHandle, tpm2.Policy(ekTemplate.NameAlg, 16, tpmutil.EkPolicyCallback)),
 //		truncated...
 //	}.Execute(tpm)
-func EkPolicyCallback(t transport.TPM, handle tpm2.TPMISHPolicy, nonceTPM tpm2.TPM2BNonce) error {
+func EkPolicyACallback(t transport.TPM, handle tpm2.TPMISHPolicy, nonceTPM tpm2.TPM2BNonce) error {
+	// Satisfy PolicyA by calling [tpm2.PolicySecret] on [tpm2.TPMRHEndorsement].
 	cmd := tpm2.PolicySecret{
 		AuthHandle:    tpm2.TPMRHEndorsement,
 		PolicySession: handle,
 		NonceTPM:      nonceTPM,
 	}
 	_, err := cmd.Execute(t)
+	return err
+}
+
+// EkPolicyBViaACallback is a callback used to satisfy EK's PolicyB via PolicyA.
+// It is used to create a policy session for the endorsement hierarchy.
+//
+// Example:
+//
+//	activateRsp, err := tpm2.ActivateCredential{
+//		KeyHandle: tpmutil.ToAuthHandle(ekHandle, tpm2.Policy(ekTemplate.NameAlg, 16, tpmutil.EkPolicyBViaACallback(ekTemplate.NameAlg))),
+//		truncated...
+//	}.Execute(tpm)
+func EkPolicyBViaACallback(alg tpm2.TPMIAlgHash) tpm2.PolicyCallback {
+	return func(t transport.TPM, handle tpm2.TPMISHPolicy, nonceTPM tpm2.TPM2BNonce) error {
+		if err := EkPolicyACallback(t, handle, nonceTPM); err != nil {
+			return err
+		}
+		return ekPolicyB(t, handle, alg)
+	}
+}
+
+// Satisfy PolicyB by calling [tpm2.PolicyOr] with PolicyA and PolicyC.
+func ekPolicyB(t transport.TPM, handle tpm2.TPMISHPolicy, alg tpm2.TPMIAlgHash) error {
+	policyA, _ := authPolicyA(alg)
+	policyC, _ := authPolicyC(alg)
+	digests := []tpm2.TPM2BDigest{policyA, policyC}
+
+	policyOrCmd := tpm2.PolicyOr{
+		PolicySession: handle,
+		PHashList:     tpm2.TPMLDigest{Digests: digests},
+	}
+	_, err := policyOrCmd.Execute(t)
 	return err
 }
